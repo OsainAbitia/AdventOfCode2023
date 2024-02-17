ab9eb4bc93249acfc0f7d53164b9e3b22ec7adcc
Osain Abitia <osain.abitia@gmail.com>
2024-02-16 21:29:23 -0600

    Day 5 part 1 input

---
+++ day-5/part-1/seed_2_fertilizer.py
@@ -0,0 +1,6 @@
+# 1. Read the data and segregate the seeds and the soil, fertilizer, etc. mapping
+# 2. Iterating over the seeds, start going through the relation mapping and find
+#    the range where the current seed falls. The range is obtained by summing the
+#    source range start + range length.
+if __name__ == '__main__':
+    pass
